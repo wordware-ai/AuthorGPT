8a6d53aef2cc2f167d9c7cc90b8213d2785f7b21
robertchandler <robert.j.h.chandler@googlemail.com>
2023-10-11 11:55:23 -0700

    Placeholder landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,229 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { CogIcon, GhostIcon, HeartIcon, Rocket, SirenIcon, Swords } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between w-full">
+      <section className="bg-white dark:bg-gray-900">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-6">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
+              Write a whole book in{" "}
+              <span className="from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">
+                minutes
+              </span>
+            </h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              Any style, any storyline. What are you waiting for?
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            >
+              Get started
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-6 lg:flex">
+            <Image src="/books.png" alt="a pile of books" width="1920" height="1080" />
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+          <h2 className="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">
+            Some of our creations
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="grid gap-8 text-gray-500 sm:gap-12 md:grid-cols-2 lg:grid-cols-4 dark:text-gray-400">
+            <div>
+              <div className="flex justify-center items-center mb-4 w-full h-36 bg-amber-100"></div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Title 1</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing
+                goals every month with our marketing plan.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-full h-36 bg-amber-100"></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Title 2</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing
+                goals every month with our marketing plan.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-full h-36 bg-amber-100"></div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Title 3</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing
+                goals every month with our marketing plan.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-full h-36 bg-amber-100"></div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Title 4</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing
+                goals every month with our marketing plan.
+              </p>
+            </div>
+          </div>
+          <div className="grid mt-20">
+            <a
+              href="#"
+              className=" mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              Start my book
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section
+        className="
+      bg-gray-50 dark:bg-gray-800"
+      >
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div className="max-w-screen-md mb-8 lg:mb-16">
+            <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">You are the artist</h2>
+            <p className="text-gray-500 sm:text-xl dark:text-gray-400">
+              Choose how you want you book to be written and make it personal...
+            </p>
+          </div>
+          <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                <Swords className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Fantasy</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing
+                goals every month with our marketing plan.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                <Rocket className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Sci-Fi</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Protect your organization, devices and stay compliant with our structured workflows and custom
+                permissions made for you.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                <HeartIcon className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Romance</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to
+                help you get started.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                <GhostIcon className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Horror</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Audit-proof software built for critical financial operations like month-end close and quarterly
+                budgeting.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                <SirenIcon className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Crime</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Craft beautiful, delightful experiences for both marketing and product with real cross-company
+                collaboration.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                <CogIcon className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Advanced</h3>
+              <p className="text-gray-500 dark:text-gray-400">Something about complete customisation</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-white dark:bg-gray-900">
+        <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+          <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">Some heading</h2>
+            <p className="mb-4">Some paragraph.</p>
+            <p>Some other paragraph. Make people want to create</p>
+            <div className="mt-10">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Begin
+              </a>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4 mt-8">
+            <Image
+              className="w-full rounded-lg aspect-[3/4] object-cover"
+              src="/typewriter.avif"
+              alt="a blank page"
+              width="300"
+              height="400"
+            />
+            <Image
+              className="mt-4 w-full lg:mt-10 rounded-lg aspect-[3/4] object-cover"
+              src="/paper.avif"
+              alt="typewriter"
+              width="300"
+              height="400"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="bg-[url('/keys.avif')] w-full bg-center h-fit bg-cover">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm text-center">
+            <h2 className="mb-4 text-4xl font-extrabold leading-tight text-white">Let&apos;s write.</h2>
+            <p className="mb-6 font-light text-gray-300 md:text-lg">What&apos;s your story?</p>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              Make me an author
+            </a>
+          </div>
+        </div>
+      </section>
+      <footer className="p-4 bg-white w-full">
+        <div className="mx-auto w-fit">
+          <p className="font-semibold uppercase text-center">Powered by</p>
+          <div className="max-w-md">
+            <a href="https://wordware.ai" target="_blank">
+              <Image src="/logo_black.svg" alt="Wordware logo" width="1000" height="400" />
+            </a>
+          </div>
+        </div>
+      </footer>
     </main>
-  )
+  );
 }
