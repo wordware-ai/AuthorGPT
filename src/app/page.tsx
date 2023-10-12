2c869bfea57232495d1cd9ab42144d5498c1d436
robertchandler <robert.j.h.chandler@googlemail.com>
2023-10-12 11:44:45 -0700

    Add sparkles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
           <h2 className="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">
-            Some of our creations
+            ✨ Some of our creations ✨
           </h2>
           <div className="grid gap-8 text-gray-500 sm:gap-12 md:grid-cols-2 lg:grid-cols-4 dark:text-gray-400">
             <div>
