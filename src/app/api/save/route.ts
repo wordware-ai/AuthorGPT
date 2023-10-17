938182dcd5b59387ec3b7b5c6c9febc1e8ed6ebe
robertchandler <robert@heydaily.co>
2023-10-17 00:14:14 -0700

    Actually log the body

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -5,7 +5,7 @@ export const runtime = "edge";
 export async function POST(req: Request): Promise<Response> {
   // TODO: Implement save to db
   console.log("Got request", req);
-  console.log("Body", req.body);
+  console.log("Body", await req.json());
   const id = uuid.v4();
   return new Response(
     JSON.stringify({
