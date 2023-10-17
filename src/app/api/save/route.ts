701bb4c26a8fc3be82906fb1a127d2099d148866
robertchandler <robert@heydaily.co>
2023-10-17 16:36:17 -0700

    Make the log short enough to be logged

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -5,8 +5,16 @@ export const runtime = "edge";
 export async function POST(req: Request): Promise<Response> {
   // TODO: Implement save to db
   console.log("Got request", req);
-  console.log("Body", await req.json());
+  const { email, genre, prompt, style, title, outline, chapters } = await req.json();
+  console.log("Email", email);
+  console.log("Genre", genre);
+  console.log("Prompt", prompt);
+  console.log("Style", style);
+  console.log("Title", title);
+  console.log("Outline", outline);
+  console.log("Chapters", chapters);
   const id = uuid.v4();
+  console.log("id", id);
   return new Response(
     JSON.stringify({
       ok: true,
