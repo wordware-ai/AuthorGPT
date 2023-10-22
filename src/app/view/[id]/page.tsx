909c6ff8058c0b2c0769dcd448d75bc57867ce93
robertchandler <robert@heydaily.co>
2023-10-22 10:04:26 -0700

    Add a basic book viewer

---
+++ src/app/view/[id]/page.tsx
@@ -0,0 +1,110 @@
+import { WordwareFooter } from "@/components/custom/footer";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { db } from "@/db/db";
+import { eq } from "drizzle-orm";
+import { books } from "@/db/schema";
+import { NotFoundError } from "@/components/custom/NotFoundError";
+
+interface BookContent {
+  [key: string]: { title: string; content: string };
+}
+
+async function DisplayBook({ id }: { id: string }) {
+  const book = await db.query.books.findFirst({ where: eq(books.id, id) });
+
+  if (!book) {
+    return <NotFoundError />;
+  }
+
+  const content = book.content as BookContent;
+
+  return (
+    <>
+      <h1 className="mb-8 text-5xl font-extrabold leading-none md:text-6xl xl:text-7xl dark:text-white">
+        {book?.title}
+      </h1>
+      {Object.entries(content)
+        .sort((a, b) => parseInt(a[0]) - parseInt(b[0]))
+        .map(([chapterNumber, { title, content }]) => (
+          <div key={chapterNumber}>
+            <h2 className="mb-4 text-2xl font-extrabold leading-none md:text-3xl xl:text-3xl dark:text-white">
+              Chapter {chapterNumber}: {title}
+            </h2>
+            <p className="whitespace-break-spaces mb-8">{content}</p>
+          </div>
+        ))}
+    </>
+  );
+}
+
+export default async function View({ params }: { params: { id: string } }) {
+  const ContentLoader = () => (
+    <div className="w-full">
+      <Skeleton className="h-12 mb-2 w-full max-w-screen-md" />
+      <Skeleton className="h-12 mb-8 w-36" />
+      <Skeleton className="h-8 w-48 mb-4" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[98%]" />
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[50%]" />
+      </div>
+      <div className="space-y-2 mt-8">
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[98%]" />
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[50%]" />
+      </div>
+      <div className="space-y-2 mt-8">
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[98%]" />
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[50%]" />
+      </div>
+      <div className="space-y-2 mt-8">
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[98%]" />
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[50%]" />
+      </div>
+      <div className="space-y-2 mt-8">
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[98%]" />
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[50%]" />
+      </div>
+      <div className="space-y-2 mt-8">
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[98%]" />
+        <Skeleton className="h-4 w-[95%]" />
+        <Skeleton className="h-4 w-[100%]" />
+        <Skeleton className="h-4 w-[50%]" />
+      </div>
+    </div>
+  );
+
+  return (
+    <main className="flex min-h-screen flex-col w-full bg-white dark:bg-gray-900">
+      <div className="max-w-screen-xl w-full px-4 pt-8 mx-auto xl:gap-0 lg:pt-16">
+        <Suspense fallback={<ContentLoader />}>
+          <DisplayBook id={params.id} />
+        </Suspense>
+      </div>
+      <div className="mt-auto">
+        <WordwareFooter />
+      </div>
+    </main>
+  );
+}
