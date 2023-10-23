2aec70498f255495106e29c7cc2c4c92c9a772ce
robertchandler <robert@heydaily.co>
2023-10-22 22:44:08 -0700

    Move analytics inside the body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Analytics />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
