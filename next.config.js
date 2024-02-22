dcc2a73c9fc13ca8dc83682d13a83808d9e4d2dd
robertchandler <robert@heydaily.co>
2024-02-22 16:50:54 +0000

    Allow cnd.wordware.ai images

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "storage.mystic.ai",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.wordware.ai",
+      },
     ],
   },
 };
