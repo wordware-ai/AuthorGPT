76e118fb2e8380679df084722d2ff6e0d1b93b1d
robertchandler <robert@heydaily.co>
2024-02-22 15:05:06 +0000

    Throw an error if the image fails to generate

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -127,8 +127,7 @@ export async function POST(req: Request): Promise<Response> {
 
     if (!image || image === "" || image === "Service Unavailable" || image.includes("Cannot read properties of null")) {
       console.error("Something went wrong generating image");
-      // Don't throw an error if the image fails to generate
-      // throw Error("Failed to generate image");
+      throw Error("Failed to generate image");
     } else {
       await db
         .update(books)
